70601c4d9e94a56838fdd88186b7085d46f223ff
gadereshma5 <38101853+gadereshma5@users.noreply.github.com>
2018-04-25 17:57:19 +0530

    Add files via upload

--- For_Loop.java
+++ For_Loop.java
@@ -3,11 +3,16 @@ package First_package;
 public class For_Loop {
 
 	public static void main(String[] args) {
-		
+		int sum=0;
 		int i;
 		for(i=0;i<=10;i++)
 		{
 			System.out.println("The number is "+i);
+		}
+		for(i=0;i<=20;i++)
+		{
+			sum=sum+i;
+			System.out.println("sum="+sum);
 		}
 	}
 
